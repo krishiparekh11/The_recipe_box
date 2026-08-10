5c0fd4e656128bda2d5796352125ec3008e1c8ca
krishi <parekh.krishi@northeastern.edu>
2026-08-10 14:19:34 -0400

    create id
    create id

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -5,7 +5,7 @@ const c = require("../controllers/recipeController");
 
 router.get("/", c.listRecipes);              // list + search + category + favorites filters
 router.get("/:id", c.getRecipe);             // recipe details
-//router.post("/", c.createRecipe);            // add a recipe 
+router.post("/", c.createRecipe);            // add a recipe 
 router.put("/:id", c.updateRecipe);          // edit a recipe
 router.patch("/:id/favorite", c.toggleFavorite); // favorite / unfavorite
 //router.delete("/:id", c.deleteRecipe);       // delete a recipe
