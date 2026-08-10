8ca6ef5032030aa74bbb1bc7bbda25daf7a132fa
krishi <parekh.krishi@northeastern.edu>
2026-08-10 14:19:10 -0400

    Update recipes.js

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -5,9 +5,9 @@ const c = require("../controllers/recipeController");
 
 router.get("/", c.listRecipes);              // list + search + category + favorites filters
 router.get("/:id", c.getRecipe);             // recipe details
-router.post("/", c.createRecipe);            // add a recipe
+//router.post("/", c.createRecipe);            // add a recipe 
 router.put("/:id", c.updateRecipe);          // edit a recipe
 router.patch("/:id/favorite", c.toggleFavorite); // favorite / unfavorite
-router.delete("/:id", c.deleteRecipe);       // delete a recipe
+//router.delete("/:id", c.deleteRecipe);       // delete a recipe
 
 module.exports = router;
